30b973617a5bef5b57d2ebd080ae43fbd6263b56
André Roberto <andreroberto52@gmail.com>
2019-04-23 20:22:56 -0300

    Carga inicial

---
+++ index.js
@@ -0,0 +1,15 @@
+class Conta {
+  constructor(nome,dataVencimento,valor)
+  {
+    this.nome = nome;
+    this.dataVencimento = dataVencimento;
+    this.valor = valor;
+  }
+}
+
+function push()
+{
+var nomeConta = document.getElementById('nomeConta');
+conta = new Conta(nomeConta);
+console.log(nomeConta.value);
+}
