6f21503344be60a137487a9b5541d4e7e7c85720
André Roberto <andreroberto52@gmail.com>
2019-04-30 19:27:31 -0300

    Ajustes finais
    Finalização do ajuste de layout

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 class Conta {
-  constructor(nome,dataVencimento,valor)
-  {
+  constructor(nome,dataVencimento,valor){
     this.nome = nome;
     this.dataVencimento = dataVencimento;
     this.valor = valor;
   }
 }
 
-function push()
-{
+function push(){     
 var nomeConta = document.getElementById('nomeConta');
-conta = new Conta(nomeConta);
+conta = new Conta(nomeConta)
 console.log(nomeConta.value);
 }
